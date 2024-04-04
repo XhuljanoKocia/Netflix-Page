5facfaa4ed1fa982642e047846c76fa2a8d0ca3d
Xhuljano Kocia <Xhuljano.Kocia@boc-group.com>
2024-04-04 14:51:46 +0200

    Lets try the login function out

--- login.php
+++ login.php
@@ -1,9 +1,16 @@
 <?php
+    require_once("includes/config.php");
+    require_once("includes/classes/FormSanitizer.php");
+    require_once("includes/classes/Constants.php");
+    require_once("includes/classes/Account.php");
+
+    $account = new Account($con);
+
     if(isset($_POST["submitButton"])){
         $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
         $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
 
-        $success = $account->register($username, $password);
+        $success = $account->login($username, $password);
 
         if($success) {
             header("Location: index.php");
@@ -33,6 +40,7 @@
             </div>
 
             <form method="POST">
+                <?php echo $account->getError(Constants::$loginFailed); ?>
                 <input type="text" name="username" placeholder="Username" required>
                 <input type="password" name="password" placeholder="Password" required>
                 <input type="submit" name="submitButton" value="SUBMIT">
