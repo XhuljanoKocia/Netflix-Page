ca8f3287a7fbbe120afae443b3ed33bd6353474d
xkocia <xhuljano.kocia@boc-group.com>
2024-05-28 16:49:35 +0200

    Creating the season provider

---
+++ includes/classes/SeasonProvider.php
@@ -0,0 +1,12 @@
+<?php
+    class SeasonProvider {
+        private $con, $username;
+
+        public function __construct($con, $username) {
+            $this->con = $con;
+            $this->username = $username;
+        }
+
+        public function creat($entity) {}
+    }
+?>
