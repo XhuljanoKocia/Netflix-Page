c0bc57ceaa414d24955573a9fcf4bc2a01839071
xkocia <xhuljano.kocia@boc-group.com>
2024-05-29 11:53:51 +0200

    Creating the season class

--- entity.php
+++ entity.php
@@ -4,13 +4,12 @@
     if(!isset($_GET["id"])) {
         ErrorMessage::show("No ID passed into page");
     }
-
     $entityId = $_GET["id"];
     $entity = new Entity($con, $entityId);
-
+    
     $preview = new PreviewProvider($con, $userLoggedIn);
     echo $preview->createPreviewVideo($entity);
-
+    
     $seasonProvider = new SeasonProvider($con, $userLoggedIn);
     echo $seasonProvider->create($entity);
 ?>
--- includes/classes/Entity.php
+++ includes/classes/Entity.php
@@ -37,7 +37,7 @@
         public function getSeasons() {
             $query = $this->con->prepare("SELECT * FROM videos WHERE entityId=:id
                                           AND isMovie=0 ORDER BY season, episode ASC");
-            $query->bindValue(":id", $entity->getId());
+            $query->bindValue(":id", $this->getId());
             $query->execute();
 
             $seasons = array();
@@ -45,9 +45,21 @@
             $currentSeason = null;
 
             while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+
+                if($currentSeason != null && $currentSeason != row["season"]) {
+                    $seasons[] = new Season($currentSeason, $videos);
+                    $video = array();
+                }
+
                 $currentSeason = $row["season"];
                 $videos[] = new Video($this->con, $row);
             }
+
+            if(sizeof($videos) != 0) {
+                $seasons[] = new Season($currentSeason, $videos);
+            }
+
+            return $seasons;
         }
     }
 ?>
