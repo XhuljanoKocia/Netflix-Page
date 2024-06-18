726c6773023885727c4225ca36242a57f44f7b71
xkocia <xhuljano.kocia@boc-group.com>
2024-06-18 17:15:48 +0200

    Outputting the episodes

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -17,10 +17,16 @@
             $seasonsHtml = "";
             foreach($seasons as $season) {
                 $seasonNumber = $season->getSeasonNumber();
+
+                $videosHtml = "";
+                foreach($season->getVideos() as $video) {
+                    $videosHtml .= $this->createVideoSquare($video);
+                }
     
     
                 $seasonsHtml .= "<div class='season'>
                                         <h3>Season $seasonNumber</h3>
+                                        <div class='videos'>$videosHtml</div>
                                     </div>";
             }
     
@@ -33,6 +39,21 @@
             $title = $video->getTitle();
             $description = $video->getDescription();
             $episodeNumber = $video->getEpisodeNumber();
+
+            return "<a href='watch.php?id=$id'>
+                        <div class='episodeContainer'>
+                            <div class='contents'>
+
+                                <img src='$thumbnail'>
+
+                                <div class='videoInfo'>
+                                    <h4>$title</h4>
+                                    <span>$description</span>
+                                </div>
+
+                            </div>
+                        </div>
+                    </a>";
         }
     }
 ?>
