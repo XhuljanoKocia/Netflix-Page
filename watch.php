743edda5b60962649cb74ca192f9242b815c63f8
xkocia <xhuljano.kocia@boc-group.com>
2024-06-25 14:53:05 +0200

    Back button functionality

--- watch.php
+++ watch.php
@@ -12,7 +12,7 @@
 <div class="watchContainer">
 
     <div class="videoControls watchNav">
-        <button><i class="fa-solid fa-arrow-left"></i></button>
+        <button onclick="goBack()"><i class="fa-solid fa-arrow-left"></i></button>
         <h1><?php echo $video->getTitle(); ?></h1>
     </div>
 
