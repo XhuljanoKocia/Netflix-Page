59cf249f6abea673bee96116416dd3dc5d04a27a
xkocia <xhuljano.kocia@boc-group.com>
2024-07-01 13:30:41 +0200

    Passing data in our ajax call

--- ajax/addDuration.php
+++ ajax/addDuration.php
@@ -1,3 +1,10 @@
 <?php
-    
+    require_once("../includes/config.php");
+
+    if(isset($_POST["videoId"]) && isset($_POST["username"])) {
+
+    }
+    else {
+        echo "No videoId or username passed into file";
+    }
 ?>
