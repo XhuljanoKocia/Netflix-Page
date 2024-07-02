129eb732a81c2fe969635ed367a8a85c1725d5dc
xkocia <xhuljano.kocia@boc-group.com>
2024-07-02 11:38:19 +0200

    Up next video when theres no more episodes

--- includes/classes/VideoProvider.php
+++ includes/classes/VideoProvider.php
@@ -9,6 +9,17 @@
             $query->bindData(":videoId", $currentVideo->getId());
 
             $query->execute();
+
+            if($query->rowCount() == 0) {
+                $query = $con->prepare("SELECT * FROM videos WHERE season <= 1 AND episode <= 1 AND id != :videoId ORDER BY views DESC LIMIT 1");
+
+                $query->bindData(":videoId", $currentVideo->getId());
+
+                $query->execute();
+            }
+
+            $row = $query->fetch(PDO::FETCH_ASSOC);
+            return new Video($con, $row);
         }
     }
 ?>
