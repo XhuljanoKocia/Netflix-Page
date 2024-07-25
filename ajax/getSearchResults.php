8b8fa868cc76dc13fe19d2d1c96b772c3346f9b5
xkocia <xhuljano.kocia@boc-group.com>
2024-07-25 15:43:49 +0200

    Creating the search results provider

--- ajax/getSearchResults.php
+++ ajax/getSearchResults.php
@@ -1,8 +1,10 @@
 <?php
     require_once("../includes/config.php");
+    require_once("../includes/classses/SearchResultsProvider.php");
 
     if(isset($_POST["term"]) && isset($_POST["username"])) {
-        echo "Hello " . $_POST["term"];
+        $srp = new SearchResultsProvider($con, $_POST["username"]);
+        echo $srp->getResults($_POST["term"]);
     }
     else {
         echo "No term or username passed into file";
